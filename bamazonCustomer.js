f9a9022d4780eb99a5294f78926a677f27cd4825
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-10-28 06:02:58 -0400

    added constructor for chosen item, function to select quantity of chosen item (cost calculation must be fixed).

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,17 @@ var mysql = require ('mysql');
 var inquirer = require ('inquirer');
 var selectionArry = [];
 
+    function ChosenItem(id,name, qtyAvailable, price) {
+        this.id = id,
+        this.name = name,
+        this.qtyAvailable = qtyAvailable,
+        this.price = price
+        // this.qtySelected = qtySelected,
+        // this.cost =  this.qtySelected * this.price
+        }
+
+
+
 var connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
@@ -55,12 +66,15 @@ function inventoryBrowse(){
             connection.query(query, {product_name: answer.selection}, function(err, res) {
                 if (err) throw err;
                 for (i = 0; i < res.length; i++) {
-                    console.log("product: " + res[i].product_name + " || Quantity in Stock: " + res[i].stock_quantity + " || Price: " + res[i].price);
+                    console.log("Product: " + res[i].product_name + " || Quantity in Stock: " + res[i].stock_quantity + " || Price: " + res[i].price);
                     if(res[i].stock_quantity === 0){
                         console.log("sorry, that product is sold out.");
                         inventoryBrowse();
                     }
-                    else selectQuantity(res);
+                    else {
+                       var newChosenItem = new ChosenItem(res[i].item_id, res[i].product_name, res[i].stock_quantity, res[i].price);
+                        selectQuantity(newChosenItem)
+                    };
                 };
                 
             });
@@ -94,10 +108,11 @@ function inventoryBrowse(){
              message: "How many do you want to order?",
             }
         ]).then(answer => {
-            var query = "SELECT "
-            if (answer.selectQuantity <= res[i].stock_quantity){
-               var cost = res[i].stock_quantity * res[i].price;
-               console.log("Your price will be", cost);
+            // var query = "SELECT product_name, stock_quantity FROM products"
+            if (answer.quantityRequest <= this.qtyAvailable){
+                this.qtySelected === answer.selectQuantity;
+                this.cost = this.qtySelected * this.price;
+               console.log("Your price will be", this.cost);
             }
         });
      };          
