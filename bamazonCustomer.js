94f778abd50813106dc5a7553890326d0821c3df
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-10-29 00:35:54 -0400

    modified the inventoryBrowse function to use constructor and compare quantities

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,16 +2,15 @@ var mysql = require ('mysql');
 var inquirer = require ('inquirer');
 var selectionArry = [];
 
-    function ChosenItem(id,name, qtyAvailable, price) {
+    function ChosenItem (id, name, qtyAvailable, price) {
         this.id = id,
         this.name = name,
         this.qtyAvailable = qtyAvailable,
         this.price = price
         // this.qtySelected = qtySelected,
         // this.cost =  this.qtySelected * this.price
-        }
-
-
+        };
+var newChosenItem = new ChosenItem(0, "test", 0, 0);
 
 var connection = mysql.createConnection ({
     host: "localhost",
@@ -32,7 +31,6 @@ var displayInventory = function(){
     connection.query(query, function(err, res){
         if (err) throw err;
         console.log("Inventory available for purchase\n");
-       
     //    res.forEach(e => console.log("Item ID:", e.item_id,"| Product Name: ", e.product_name, "| Department: ", e.department_name, "| Price: ", e.price, "| Stock Quantity: ", e.stock_quantity, "\n"));
        for (i = 0; i < res.length; i++) {
         console.log("Item ID:", res[i].item_id,"| Product Name: ", res[i].product_name, "| Department: ", res[i].department_name, "| Price: ", res[i].price, "| Stock Quantity: ", res[i].stock_quantity, "\n");
@@ -60,8 +58,12 @@ function inventoryBrowse(){
          message: "Please select the product that you will purchase.",
          choices: selectionArry
         }
+        // {
+        //     name: "quantityRequest",   
+        //     type: "intput",
+        //     message: "How many do you want to order?",
+        //    }
     ]).then(answer => 
-        // console.log ("You chose", answer)
             {var query = "SELECT product_name, stock_quantity, price FROM products WHERE ?";
             connection.query(query, {product_name: answer.selection}, function(err, res) {
                 if (err) throw err;
@@ -72,8 +74,10 @@ function inventoryBrowse(){
                         inventoryBrowse();
                     }
                     else {
-                       var newChosenItem = new ChosenItem(res[i].item_id, res[i].product_name, res[i].stock_quantity, res[i].price);
-                        selectQuantity(newChosenItem)
+                       var newChosenItem2 = new ChosenItem(res[i].item_id, res[i].product_name, res[i].stock_quantity, res[i].price);
+                       newChosenItem = newChosenItem2;
+                       console.log("new Item: ", newChosenItem);
+                        selectQuantity();
                     };
                 };
                 
@@ -82,19 +86,6 @@ function inventoryBrowse(){
 
     );
             
-            
-            
-        // {
-        //  type: "input",
-        //  name: "selecionQuantity",
-        //  message: "How many do you wish to order?",
-
-        // }
-        // ).then(function(answer){
-        //     console.log("You chose", selectionQuantity.answer, selection.answer)
-
-        // })
-
     };
     // connection.end();
 
@@ -104,15 +95,41 @@ function inventoryBrowse(){
         inquirer.prompt ([
             {
              name: "quantityRequest",   
-             type: "intput",
+             type: "number",
              message: "How many do you want to order?",
             }
-        ]).then(answer => {
-            // var query = "SELECT product_name, stock_quantity FROM products"
-            if (answer.quantityRequest <= this.qtyAvailable){
-                this.qtySelected === answer.selectQuantity;
-                this.cost = this.qtySelected * this.price;
-               console.log("Your price will be", this.cost);
-            }
+            ]).then(answer => {
+                if(answer.quantityRequest > newChosenItem.qtyAvailable){
+                    console.log("Sorry, there are only", newChosenItem.qtyAvailable, "in stock.\n Please order again.");
+                    selectQuantity();
+                }
+
+                if (answer.quantityRequest <= newChosenItem.qtyAvailable){
+                    this.qtySelected;
+                    this.cost;
+                    this.qtySelected = answer.quantityRequest;
+                    this.cost = this.qtySelected * newChosenItem.price;
+                    console.log("Your price will be", this.cost);
+                    console.log("this cost is a ", typeof(this.cost))
+                    updateQty();
+                };
+        
         });
-     };          
+    }
+
+    function updateQty(){
+        newChosenItem.qtyAvailable =- answer.quantityRequest;
+        connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+              stock_quantity: newChosenItem.qtyAvailable
+            },
+            {
+              id: chosenItem.id
+            }
+          ]),
+          function(error) {
+            if (error) throw err;
+            console.log("Amt. left in stock:", stock_quantity );
+        };
+    };
